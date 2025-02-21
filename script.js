4a92b9b5c3f5f997f90c21f0632532a0a0ea8b7e
Jegrid <jed.casey@outlook.com>
2025-02-21 14:31:36 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
 document.addEventListener("DOMContentLoaded", function () {
   const bgImage = new Image();
   bgImage.onload = function () {
-    document.body.style.backgroundImage = url(
-      "https://front-end-fundamentals-2025.github.io/fef-project-group-37/images/homeimage.png"
-    );
+    document.body.style.backgroundImage =
+      "url('https://front-end-fundamentals-2025.github.io/fef-project-group-37/images/homeimage.png')";
     document.body.style.backgroundSize = "cover";
   };
   bgImage.src =
