f275ebf80a44ba80a08b1c472587538ea0fe2f62
Jegrid <jed.casey@outlook.com>
2025-02-21 14:36:01 +0100

    image load test

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
-document.addEventListener("DOMContentLoaded", function () {
+window.onload = function () {
+  console.log("Script is running...");
+
   const bgImage = new Image();
   bgImage.onload = function () {
+    console.log("Image loaded");
     document.body.style.backgroundImage =
-      "url('https://front-end-fundamentals-2025.github.io/fef-project-group-37/images/homeimage.png')";
+      "url('https://yourusername.github.io/your-repository/images/homeimage.png')";
     document.body.style.backgroundSize = "cover";
   };
   bgImage.src =
-    "https://front-end-fundamentals-2025.github.io/fef-project-group-37/images/homeimage.png";
-});
+    "https://yourusername.github.io/your-repository/images/homeimage.png";
+};
